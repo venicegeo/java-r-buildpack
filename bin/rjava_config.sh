d1eccc76500a210f3a1c47ecb0f88c338478400e
malston <mark_alston@yahoo.com>
2014-03-20 12:08:18 -0600

    Testing

--- bin/rjava_config.sh
+++ bin/rjava_config.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+echo "Calling 'R CMD javareconf' . . ."
+
+GCC_VERSION="4.3"
+
+export R_BASE=/app/vendor/R
+export R_HOME=$R_BASE/lib64/R
+export JAVA_HOME=/app/vendor/openjdk-1.7.0_51
+export JAVA_CPPFLAGS="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
+export R_INCLUDE=$R_HOME/include
+export PATH=$JAVA_HOME/bin:$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+
 export JAVA_HOME=/app/vendor/openjdk-1.7.0_51
 /app/vendor/R/bin/R CMD javareconf
